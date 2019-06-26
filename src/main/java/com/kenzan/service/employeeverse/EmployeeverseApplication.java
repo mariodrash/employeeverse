47c140c1bb3a90eea5193b5ebdad0fb85a79d714
Mario Rodríguez Portillo <mkernel@Marios-MBP.local>
2019-06-26 14:48:54 -0500

    Starting API Service Repository 🍺

---
+++ src/main/java/com/kenzan/service/employeeverse/EmployeeverseApplication.java
@@ -0,0 +1,13 @@
+package com.kenzan.service.employeeverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenzan/service/employeeverse/EmployeeverseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kenzan.service.employeeverse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeverseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
